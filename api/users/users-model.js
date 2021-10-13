db891651544013921af23b9516908641c14d0b06
stevenjhomem <sjhomem@outlook.com>
2021-10-13 15:46:31 -0500

    finished findbyid

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,31 +1,6 @@
 const db = require("../../data/db-config.js");
 
 function find() {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users.
-
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "role_name": "admin"
-      },
-      {
-        "user_id": 2,
-        "username": "sue",
-        "role_name": "instructor"
-      }
-    ]
-   */
-
-  //     select
-  //     user_id,
-  //     username,
-  //     role_name
-  // from users
-  // join roles on
-  //     users.role_id=roles.role_id
   return db("users")
     .join("roles", "users.user_id", "roles.role_id")
     .select("user_id", "username", "role_name");
@@ -45,19 +20,18 @@ function findBy(filter) {
       }
     ]
    */
+  return db("users")
+    .join("roles", "users.user_id", "roles.role_id")
+    .select("user_id", "username", "role_name", "password")
+    .where(filter);
 }
 
 function findById(user_id) {
-  /**
-    You will need to join two tables.
-    Resolves to the user with the given user_id.
-
-    {
-      "user_id": 2,
-      "username": "sue",
-      "role_name": "instructor"
-    }
-   */
+  return db("users")
+    .join("roles", "users.user_id", "roles.role_id")
+    .select("user_id", "username", "role_name")
+    .where("users.user_id", user_id)
+    .first();
 }
 
 /**
