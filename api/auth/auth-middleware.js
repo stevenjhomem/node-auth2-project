29b93c8386d0604d3a6bdb3f8d88fd911d298a2e
stevenjhomem <sjhomem@outlook.com>
2021-10-13 19:46:39 -0500

    almost done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,22 +1,20 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const { findBy } = require("../users/users-model");
+const jwt = require("jsonwebtoken");
 const restricted = (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
+ 
+  const token = req.headers.authorization;
+  if (!token) {
+    return next({ status: 401, message: "Token required" });
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      next({ status: 401, message: "Token invalid" });
+    } else {
+      req.decodedToken = decodedToken;
+      next();
     }
-
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
-  next();
+  });
 };
 
 const only = (role_name) => (req, res, next) => {
@@ -41,10 +39,10 @@ const checkUsernameExists = async (req, res, next) => {
     }
   */
   try {
-    const [user] = await findBy({username: req.body.username});
+    const [user] = await findBy({ username: req.body.username });
     if (!user) {
       next({ status: 401, message: "Invalid credentials" });
-    } else{
+    } else {
       req.user = user;
       next();
     }
